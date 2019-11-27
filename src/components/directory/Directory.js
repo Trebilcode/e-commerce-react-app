a1a8f94370d295b4f029359d63a810c8eb290254
Trebilcode <d11a2n3i4e5l6@hotmail.com>
2019-11-27 18:53:36 -0500

    Update menu-items to navigate withRouter HOC

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 
 import './Directory.scss';
-import { MenuItem } from '../menu-item/MenuItem';
+import MenuItem from '../menu-item/MenuItem';
 
 export class Directory extends Component {
 
@@ -12,33 +12,35 @@ export class Directory extends Component {
         title: 'hats',
         imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
         id: 1,
-        linkUrl: 'shop/hats'
+        // linkUrl: 'shop/hats',
+        linkUrl: 'hats'
       },
       {
         title: 'jackets',
         imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
         id: 2,
-        linkUrl: 'shop/jackets'
+        // linkUrl: 'shop/jackets',
+        
       },
       {
         title: 'sneakers',
         imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
         id: 3,
-        linkUrl: 'shop/sneakers'
+        // linkUrl: 'shop/sneakers'
       },
       {
         title: 'womens',
         imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
         size: 'large',
         id: 4,
-        linkUrl: 'shop/womens'
+        // linkUrl: 'shop/womens'
       },
       {
         title: 'mens',
         imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
         size: 'large',
         id: 5,
-        linkUrl: 'shop/mens'
+        // linkUrl: 'shop/mens'
       }
     ]
   }
@@ -47,12 +49,10 @@ export class Directory extends Component {
     return(
       <div className='directory-menu'>
         {
-          this.state.sections.map(({ title, imageUrl, id, size }) =>(
+          this.state.sections.map(({ id, ...otherSectionProps }) =>(
             <MenuItem 
             key={id} 
-            title={title} 
-            imageUrl={imageUrl}
-            size={size}
+            {...otherSectionProps}
             />
             ))
         }
