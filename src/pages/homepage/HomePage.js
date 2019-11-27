1891d274b3ddfc9fb3d3c5661d5e5f0e38a2f104
Trebilcode <d11a2n3i4e5l6@hotmail.com>
2019-11-27 00:28:36 -0500

    Broken homepage into smaller components, finished styling menu-item component

---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './HomePage.scss';
+import { Directory } from '../../components/directory/Directory';
+
+export const HomePage = () => (
+  <div className='homepage'>
+    <Directory />  
+  </div>
+)
