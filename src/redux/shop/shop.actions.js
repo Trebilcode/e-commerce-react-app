807b7235ff3f92b16a17058d3102af1a8996af61
Trebilcode <d11a2n3i4e5l6@hotmail.com>
2019-12-08 21:12:13 -0500

    Installed redux-thunk to handle asynchronous code inside redux, created an async function to fetch data from database and to be called in the Shop component whit the componentDidMount lyfe cycle method

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,32 @@
 import { ShopPageActionTypes } from "./shop.types";
+import { convertCollectionsSnapShotToMap, firestore } from '../../firebase/firebase.utils';
 
-export const updateCollections = collectionsMap => ({
-  type: ShopPageActionTypes.UPDATE_COLLECTIONS,
+export const fetchCollectionsStart = collectionsMap => ({
+  type: ShopPageActionTypes.FETCH_COLLECTIONS_START
+});
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+  type: ShopPageActionTypes.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionsMap
 });
+
+export const fetchCollectionsFailure = error => ({
+  type: ShopPageActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: error
+})
+
+
+export const fetchCollectionsStartAsync = () => {
+  return dispatch => {
+    const collectionRef = firestore.collection('collections');
+
+    dispatch(fetchCollectionsStart());
+
+    collectionRef.get()
+      .then(snapShot => {
+        const collectionsMap = convertCollectionsSnapShotToMap(snapShot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));
+       })
+       .catch(error => dispatch(fetchCollectionsFailure(error.message)))
+  }
+}
